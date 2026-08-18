c1eb42aa51be5e7131e18be3ca37e59c8549f264
Fortisek90 <lg2fortuna1990@gmail.com>
2026-08-18 20:43:56 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const C='ersja-v02';const A=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
